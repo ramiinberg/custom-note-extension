e0d17b6abc23c34977d2413f99d02857632849aa
ramiinberg <rami.inberg@gmail.com>
2022-05-02 11:51:10 +0300

    Addling list items to unordered list

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ inputBtn.addEventListener("click", function() {
 })
 
 for(let i = 0; i < myNotes.length; i++) {
-  ulEl.textContent += myNotes[i] + " "
+  ulEl.innerHTML +=  "<li>" + myNotes[i] + "</li>"
 }
