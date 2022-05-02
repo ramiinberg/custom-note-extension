d26ea2c25849bc9cfbb85228f27a2d6b7839530e
ramiinberg <rami.inberg@gmail.com>
2022-05-02 11:30:08 +0300

    Added unorder list which render dummy data

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let myNotes = []
+let myNotes = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
   const inputValue = inputEl.value
   myNotes.push(inputValue)
 })
+
+for(let i = 0; i < myNotes.length; i++) {
+  ulEl.textContent += myNotes[i] + " "
+}
