914904be9b1e900ccf51e82355663abf3c1e3eff
ramiinberg <rami.inberg@gmail.com>
2022-05-02 10:55:33 +0300

    Remove old code and add event listener

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-function saveInput() {
-  console.log('Button clicked')
-}
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+  console.log("Button clicked")
+})
