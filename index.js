ed8a0edbeaf7bc4e30e0a78723b9476ea63bd1b9
ramiinberg <rami.inberg@gmail.com>
2022-05-02 17:05:42 +0300

    adding dynamic input adding

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-let myNotes = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
-let listItems = ""
+let myNotes = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-  const inputValue = inputEl.value
-  myNotes.push(inputValue)
+  myNotes.push(inputEl.value)
+  renderListItems()
 })
 
-for(let i = 0; i < myNotes.length; i++) {
-  listItems += "<li>" + myNotes[i] + "</li>"
+function renderListItems() {
+  let listItems = ""
+  for(let i = 0; i < myNotes.length; i++) {
+    listItems += "<li>" + myNotes[i] + "</li>"
+  }
+  
+  ulEl.innerHTML = listItems
 }
 
-ulEl.innerHTML = listItems
